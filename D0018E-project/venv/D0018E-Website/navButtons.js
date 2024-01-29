cec4b58c03ad575aabc9f8463484fd4e17cee08b
Lexagon02 <leo.r.bergstrom@gmail.com>
2024-01-29 16:58:02 +0100

    frontEnd

---
+++ D0018E-project/venv/D0018E-Website/navButtons.js
@@ -0,0 +1,12 @@
+function toggleRules() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function toggleHelp() {
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function toggleInfo() {
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
