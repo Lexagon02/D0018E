45ab7b7f94eeabf2fde633de9d391188cbf34133
Frikkz <106763788+Frikkz@users.noreply.github.com>
2024-01-30 15:26:08 +0100

    working

--- D0018E-project/.venv/website/__init__.py
+++ D0018E-project/.venv/website/__init__.py
@@ -7,5 +7,24 @@ def create_app():
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
+    print("hej")
+
+    import pymysql.cursors
+
+    # Connect to the database
+    connection = pymysql.connect(host='d0018e-database.cvwk6aiy4nnq.eu-north-1.rds.amazonaws.com',
+                                user='admin',
+                                password='ltu1234567',
+                                database='database',
+                                cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT *  FROM `users`"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
 
     return app
--- D0018E-project/.venv/website/views.py
+++ D0018E-project/.venv/website/views.py
@@ -4,4 +4,22 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+
+    import pymysql.cursors
+
+    # Connect to the database
+    connection = pymysql.connect(host='d0018e-database.cvwk6aiy4nnq.eu-north-1.rds.amazonaws.com',
+                            user='admin',
+                            password='ltu1234567',
+                            database='database',
+                            cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+
+        with connection.cursor() as cursor:
+             # Read a single record
+            sql = "SELECT *  FROM `tv`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+    return render_template("index.html", data=result)
