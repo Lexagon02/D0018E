8ae24d796e17e71207681915fc237cf713f63095
Frikkz <106763788+Frikkz@users.noreply.github.com>
2024-02-01 13:28:26 +0100

    implemented login and register

--- D0018E-project/.venv/website/auth.py
+++ D0018E-project/.venv/website/auth.py
@@ -1,3 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+import pymysql.cursors
+connection = pymysql.connect(host='d0018e-database.cvwk6aiy4nnq.eu-north-1.rds.amazonaws.com',
+                             user='admin',
+                             password='ltu1234567',
+                             database='database',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 auth = Blueprint('auth', __name__)
+
+
+
+@auth.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form 
+       password = request.form.get("pword") 
+       with connection:
+        with connection.cursor() as cursors:
+            # Read a single record
+            sql = "INSERT INTO users (name,surname,mail,password,address) VALUES (%s,%s,%s,%s,%s);"
+            cursors.execute(sql,(first_name,"asd","NULL",password,"NULL"))
+            connection.commit()
+            
+       
+       return "Your username is "+ first_name
+    return render_template("register.html")
+
+@auth.route("/login", methods = ["GET", "POST"])
+def login():
+    connection = pymysql.connect(host='d0018e-database.cvwk6aiy4nnq.eu-north-1.rds.amazonaws.com',
+                             user='admin',
+                             password='ltu1234567',
+                             database='database',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form 
+       password = request.form.get("pword") 
+       with connection:
+            with connection.cursor() as cursorn:
+                # Read a single record
+                sql = "SELECT `surname` FROM users WHERE name = %s AND password = %s"
+                cursorn.execute(sql,(first_name,password))
+                result = cursorn.fetchall()
+            
+       
+       return "Your id "+ str(result)
+    return render_template("login.html")
